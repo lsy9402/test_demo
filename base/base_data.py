e51eaa6f16b025b3892a4f96299e3bf9a5fa33ba
lsy9402 <452561863@qq.com>
2020-04-17 19:33:34 +0800

    first commit

--- base/base_data.py
+++ base/base_data.py
@@ -2,5 +2,5 @@ import yaml
 
 
 def get_data_yml(file_name = 'data'):
-    with open('/Users/mac/PycharmProjects/pytest_demos/data/' + file_name + '.yml', 'r') as f:
+    with open('/Users/mac/Desktop/project/data/' + file_name + '.yml', 'r') as f:
         return yaml.load(f, Loader=yaml.FullLoader)
