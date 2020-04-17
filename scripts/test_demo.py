78c58831178884cc5eb4b54859ea98c00613065e
lsy9402 <452561863@qq.com>
2020-04-17 21:44:54 +0800

    first commit

--- scripts/test_demo.py
+++ scripts/test_demo.py
@@ -1,6 +1,6 @@
 import os, sys
 
-import allure
+# import allure
 
 sys.path.append(os.getcwd())
 
@@ -19,7 +19,7 @@ class Test_demo:
         sleep(3)
         self.driver.quit()
 
-    @allure.step(title="demo的测试脚本")
+    # @allure.step(title="demo的测试脚本")
     # 设置执行顺序=1
     @pytest.mark.run(order=1)
     # 设置预言失败为False
@@ -28,9 +28,9 @@ class Test_demo:
     @pytest.mark.parametrize(["num1", "ysf", "num2", "zhi"], get_data_yml())
     def test_demo(self,num1, ysf, num2, zhi):
         # 运算8*6
-        allure.attach("测试运算是否正确", "测试")
+        # allure.attach("测试运算是否正确", "测试")
         self.demo_page.yunsuan(num1, ysf, num2)
         self.demo_page.assertin_jieguo(zhi)
 
 if __name__ == '__main__':
-    pytest.main()
+    pytest.main("-s test_demo.py")
