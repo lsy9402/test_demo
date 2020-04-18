5b2ee37272143e081bdfd9e336ceb7589e8b3bc4
lsy9402 <452561863@qq.com>
2020-04-18 12:28:14 +0800

    改base错误

--- base/base_data.py
+++ base/base_data.py
@@ -2,7 +2,7 @@ import yaml
 
 
 def get_yml_data_with_filename_key(file_name , key):
-    with open('../data/' + file_name + '.yml', 'r') as f:
+    with open('./data/' + file_name + '.yml', 'r') as f:
         data = yaml.load(f, Loader=yaml.FullLoader)[key]
         case_data_list = list()
         for case_data in data:
